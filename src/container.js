3a2dcc07a9f6b71260b3ab3cdcce50ce15aea36c
Joeri5 <joerischenk@outlook.com>
2024-05-16 14:29:19 +0200

    SCRUM-36 cleanup.js removes all javascript files created by compiling the typescript files

--- src/container.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("reflect-metadata");
-const tsyringe_1 = require("tsyringe");
-const services_1 = require("./services");
-tsyringe_1.container.register('IWebsiteCloner', { useClass: services_1.WebsiteCloner });
-tsyringe_1.container.register('IWebsiteDeployer', { useClass: services_1.WebsiteDeployer });
-tsyringe_1.container.register('IDomainManager', { useClass: services_1.DomainManager });
